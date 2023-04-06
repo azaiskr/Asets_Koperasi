b4ca58da3241e521051479a88f60fcd8e5259014
ariq <ariqfms012@students.unnes.ac.id>
2023-04-07 04:02:22 +0700

    update home, edit, sama tambah aset tersedia

--- sistemAset/resources/views/asetTersedia/AsetTersedia.blade.php
+++ sistemAset/resources/views/asetTersedia/AsetTersedia.blade.php
@@ -1,36 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Aset Tersedia</h1>
+@extends('base')
+@section('container')
+@section('title', 'Aset Tersedia')
 
-    <button class="btn btn-primary"><a href="/tambahAset">Tambah Data</a></button>
-    
+<div class="container mt-4">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/"> Home </a></li>
+            <li class="breadcrumb-item active" aria-current="page">@yield('title')</li>
+        </ol>
+    </nav>
+</div>
 
-    <table border="1">
-		<tr>
-			<th>ID ASET</th>
-			<th>NAMA ASET</th>
-			<th>STOK</th>
-		</tr>
-		@foreach($aset_tersedia as $at)
-		<tr>
-			<td>{{ $at->id_aset }}</td>
-            <td>{{ $at->nama_aset }}</td>
-            <td>{{ $at->stok }}</td>
-			<td>
-				<button class="btn btn-primary"><a href="/AsetTersedia/edit/{{$at->id_aset}}">Edit</a></button>
-				<button class="btn btn-primary"><a href="/AsetTersedia/hapus/{{$at->id_aset}}">Hapus</a></button>
-			</td>
-		</tr>
-		@endforeach
-	</table>
+<div class="container">
+    <div class="row">
+        <div class="my-4 col-12">
+            <h2 class="float-left"> Daftar Aset Tersedia </h2>
+            <a class="btn btn-primary float-right mt-2" href="/tambahTersedia" role="button">Tambah data</a>
+        </div>
+        <div class="col-md-12">
+            <table class="table table-stripped">
+                <thead class="thead-primary">
+                    <tr>
+						<th>ID ASET</th>
+						<th>NAMA ASET</th>
+						<th class="text-center">STOK</th>
 
-	<a href="/">Home</a>
-</body>
-</html>
+                </thead>
+                <tbody>
+                @foreach($aset_tersedia as $at)
+                    <tr>
+						<td>{{ $at->id_aset }}</td>
+						<td>{{ $at->nama_aset }}</td>
+						<td class="text-center">{{ $at->stok }}</td>
+						<td>
+							<a href="/AsetTersedia/edit/{{$at->id_aset}}" class="badge badge-warning">Edit</a>
+							<a href="/AsetTersedia/hapus/{{$at->id_aset}}" class="badge badge-danger">Hapus</a>
+						</td>   
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
--- sistemAset/resources/views/asetTersedia/tambahTersedia.blade.php
+++ sistemAset/resources/views/asetTersedia/tambahTersedia.blade.php
@@ -1,17 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="/AsetTersedia/store" method="post">
-		{{ csrf_field() }}
-        Nama <input type="text" name="nama_aset" required="required"> <br/>
-		Stok <input type="text" name="stok" required="required" > <br/>
-		<input type="submit" value="Simpan Data">
-	</form>
-</body>
-</html>
+@extends('base')
+@section('container')
+@section('title', 'Tambah Aset Tersedia')
+
+<div class="container mt-4">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/"> Home </a></li>
+            <li class="breadcrumb-item"><a href="AsetTersedia"> Daftar Aset Tersedia </a> </li>
+            <li class="breadcrumb-item active" aria-current="page">@yield('title')</li>
+        </ol>
+    </nav>
+</div>
+
+<div class="container"> 
+    <div class="row">
+        <div class="col-md-12 mt-3">
+            <h3 style="text-align: center"> Tambah Data Aset Tersedia </h3>
+            <form action="/AsetTersedia/store" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="nama_aset"> <h5> Nama Aset </h5> </label>
+                    <input class="form-control" type="text" name="nama_aset" required="required" placeholder="nama aset yang dipinjam">
+                    @if ($errors->has('nama_aset'))
+                        <div class="text-danger">
+                            {{$errors->first('nama_aset')}}
+                        </div>
+                    @endif
+                </div>
+                <div class="form">
+                    <label for="stok"> <h5> Stok </h5> </label><br>
+                    <input class="form-control" type="text" name="stok" id="stok" required="required" placeholder="nama peminjam">
+                    @if ($errors->has('stok'))
+                        <div class="text-danger">
+                            {{$errors->first('stok')}}
+                        </div>
+                    @endif    
+                </div>
+
+                <div class="form-group float-right">
+                    <button class="btn btn-lg btn-danger" type="reset"> Reset</button>
+                    <button class="btn btn-lg btn-success" type="sumbit"> OK </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
